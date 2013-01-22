c2d5336e2daee5fa72cba4fb728ba7bf0214d964
Tobias Günther <orkan@tobias.io>
2013-01-22 14:47:48 +0100

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Orkan',
-    version='0.1.0',
+    version='0.1.1',
     author='Tobias Guenther',
     author_email='orkan@tobias.io',
     packages=['orkan', 'orkan.test'],
